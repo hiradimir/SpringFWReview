c318ad83cc01b562129bd69897391a5588c80e0b
hiradimir <yutakachang+github@gmail.com>
2015-02-27 18:23:50 +0900

    change to aspectj-autoproxy

--- src/main/java/com/hiradimir/sample/springfwreview/web/aop/MyBeanAspect.java
+++ src/main/java/com/hiradimir/sample/springfwreview/web/aop/MyBeanAspect.java
@@ -1,14 +1,18 @@
 package com.hiradimir.sample.springfwreview.web.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by yutaka on 2015/02/27.
  */
-@Component(value = "aop1")
+@Aspect
+@Component
 public class MyBeanAspect {
     
+    @Before("execution(* com.hiradimir.sample.springfwreview.web.IMyBean.addMessage(..))")
     public void addDataBefore(JoinPoint joinPoint){
 
         System.out.println("*addData before...*");
